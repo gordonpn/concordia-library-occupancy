7ec370d516e2c9d80e2c6262769d344e23210809
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-04-05 12:05:51 -0400

    chore: remove self-implementation of logger singleton. more...
    remove configparser in favor for dotenv
    add a Dockerfile and docker-compose
    these changes are not yet tested

--- scraper/data_scraper.py
+++ scraper/data_scraper.py
@@ -1,62 +1,65 @@
+import requests
 import datetime
 import json
+import logging
 import os
 import random
 import time
+from logging.config import fileConfig
+from pathlib import Path
 from typing import Dict
 
-import requests
+from dotenv import load_dotenv
 
-from Configuration.config import Config
-from Configuration.logger import get_logger
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path, override=True)
+
+logging.config.fileConfig('logging.ini')
+logger = logging.getLogger(__name__)
 
 
 class DataCollector:
     PATH: str = 'data.json'
     URL: str = 'https://opendata.concordia.ca/API/v1/library/occupancy/'
 
-    def __init__(self):
-        self.config = Config()
-        self.logger = get_logger()
-
     def run(self):
-        self.config.load_config()
         time.sleep(self.random_delay())
         data = self.collect()
         self.save(data)
 
     def random_delay(self) -> int:
         random_time: int = random.randint(0, 30) * 60
-        self.logger.debug(f"Wait time unless next: {random_time} seconds")
+        logger.debug(f"Wait time unless next: {random_time} seconds")
         return random_time
 
     def collect(self) -> Dict[str, int]:
         data: Dict[str, int] = {}
         request_json: Dict[str, Dict[str, str]] = requests.get(
             DataCollector.URL,
-            auth=(self.config.user, self.config.key)).json()
-        self.logger.debug(f"Collected data")
+            auth=(os.getenv('OPEN_DATA_USER'), os.getenv('OPEN_DATA_KEY'))).json()
+        logger.debug(f"Collected data")
         data['Webster'] = max(0, int(float(request_json.get('Webster').get('Occupancy'))))
         data['Vanier'] = max(0, int(float(request_json.get('Vanier').get('Occupancy'))))
         data['GreyNuns'] = max(0, int(float(request_json.get('GreyNuns').get('Occupancy'))))
         return data
 
     def save(self, data):
+        # TODO modify to save into a database
         date_time: str = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M")
         if os.path.exists(DataCollector.PATH):
             try:
-                self.logger.info("Opening existing json")
+                logger.info("Opening existing json")
                 with open(DataCollector.PATH, "r") as read_file:
                     data_from_json = json.load(read_file)
             except ValueError:
-                self.logger.error("File was empty, creating new data")
+                logger.error("File was empty, creating new data")
                 data: Dict[str, float] = {}
 
             data_from_json[date_time] = data
         else:
             data_from_json = {date_time: data}
 
-        self.logger.info("Writing new data into the json")
+        logger.info("Writing new data into the json")
         with open(DataCollector.PATH, "w+") as write_file:
             json.dump(data_from_json, write_file, indent=4)
 
