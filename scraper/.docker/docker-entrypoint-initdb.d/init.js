83077f3b84a0cf60003be2f4c274f96aa9a0ec51
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-04-05 22:07:04 -0400

    feat: configure basic docker-compose and mongodb init scripts

---
+++ scraper/.docker/docker-entrypoint-initdb.d/init.js
@@ -0,0 +1,12 @@
+db.webster.insert({
+  time: new Date(),
+  occupancy: 999
+});
+db.vanier.insert({
+  time: new Date(),
+  occupancy: 666
+});
+db.greynuns.insert({
+  time: new Date(),
+  occupancy: 333
+});
