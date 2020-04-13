f5f41bc8a4a6f8b139395e7c2ce700e7c032f4d1
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-04-12 20:30:01 -0400

    ci: add slack step for notifications

--- backend/app/controller/occupancy_library.py
+++ backend/app/controller/occupancy_library.py
@@ -1,3 +1,4 @@
+from bson.json_util import dumps
 from flask_restful import Resource
 
 from ..database.database import mongo
@@ -6,4 +7,4 @@ from ..database.database import mongo
 class OccupancyLibrary(Resource):
 
     def get(self, library_name: str):
-        return mongo.db[library_name].find()
+        return dumps(mongo.db[library_name.lower()].find())
